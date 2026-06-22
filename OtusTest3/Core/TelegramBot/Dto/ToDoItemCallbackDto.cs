ceea5f9018b577352471029201312b1536fbde77
Caredr <loadko@gmail.com>
2026-06-22 17:51:44 +0000

    feat: add ToDoItemCallbackDto

--- OtusTest3/Core/TelegramBot/Dto/ToDoItemCallbackDto.cs
+++ OtusTest3/Core/TelegramBot/Dto/ToDoItemCallbackDto.cs
@@ -1,25 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace OtusTest3.Core.TelegramBot.Dto
 {
-    internal class ToDoItemCallbackDto : CallbackDto
+    public class ToDoItemCallbackDto : CallbackDto
     {
         public string Action { get; set; } = string.Empty;
         public Guid ToDoItemId { get; set; }
-        public static string ToString(ToDoItemCallbackDto dto)
+
+        // Формат: "action|toDoItemId"
+        public override string ToString()
         {
-            // Формат: "action|todoListId"
-            ArgumentNullException.ThrowIfNull(dto);
-            return $"{dto.Action}|{dto.ToDoItemId}";
+            return $"{Action}|{ToDoItemId}";
         }
-        public static ToDoItemCallbackDto FromString(string input)
+
+        public static new ToDoItemCallbackDto FromString(string input)
         {
             if (input is null)
                 throw new ArgumentNullException(nameof(input));
+
             var separatorIndex = input.IndexOf('|');
             if (separatorIndex < 0)
             {
@@ -28,9 +26,11 @@ namespace OtusTest3.Core.TelegramBot.Dto
                     Action = input
                 };
             }
+
             var action = input[..separatorIndex];
             var idPart = input[(separatorIndex + 1)..];
             var ok = Guid.TryParse(idPart, out var id);
+
             return new ToDoItemCallbackDto
             {
                 Action = action,
