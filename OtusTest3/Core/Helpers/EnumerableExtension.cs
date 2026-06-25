dc4d83f3dd286cff1c0484515a602313e1d7cfa2
Caredr <loadko@gmail.com>
2026-06-25 23:59:37 +0300

    p2

---
+++ OtusTest3/Core/Helpers/EnumerableExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusTest3.Core.Helpers
+{
+    public static class EnumerableExtension
+    {
+        public static IEnumerable<T> GetBatchByNumber<T>(
+            this IEnumerable<T> source,
+            int batchSize,
+            int batchNumber)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (batchSize <= 0)
+                throw new ArgumentOutOfRangeException(nameof(batchSize), "Размер пачки должен быть больше 0.");
+
+            if (batchNumber < 0)
+                throw new ArgumentOutOfRangeException(nameof(batchNumber), "Номер пачки не может быть меньше 0.");
+
+            return source
+                .Skip(batchSize * batchNumber)
+                .Take(batchSize);
+        }
+    }
+}
